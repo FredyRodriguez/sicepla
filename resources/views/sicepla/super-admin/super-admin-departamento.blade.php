5056912add752e7561039f467450b8db1808f6ef
EfrainAndres <efrainvergara.udec@gmail.com>
2017-08-31 11:22:18 -0500

    Datatable

--- resources/views/sicepla/super-admin/super-admin-departamento.blade.php
+++ resources/views/sicepla/super-admin/super-admin-departamento.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.dash')
+
+@push('styles')
+<!-- Datatables Styles -->
+<link href="{{ asset('assets/global/plugins/datatables/datatables.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css') }}" rel="stylesheet" type="text/css" />
+
+<link rel="stylesheet" type="text/css" href="http://cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+@endpush
+
 @section('content')
 <div class="col-md-12">
 {{--Inicio Mensaje Confirmar--}}
@@ -8,6 +20,7 @@
 {{--Fin Mensaje Confirmar--}}
     @component('components.portlet', ['icon' => 'fa fa-object-group', 'title' => 'Departamento'])
         <div id="app">
+
             {{--boton crear departamento--}}
                 <div>
                     <a  href="{{route('departamentos.create')}}" class="btn green-jungle">
@@ -15,6 +28,7 @@
                         Crear Departamento
                     </a>
                 </div>
+
             {{--fin boton crear departamento--}}
             <!-- Modal crear departamento -->
             <div id="crear-departamento" class="modal fade" role="dialog">
@@ -44,15 +58,15 @@
             <!--Fin Modal crear departamento-->
             {{--inicio tabla--}}
             <div class="table-responsive">
-                <table class="table table-hover table-bordered table-condensed">
+                <table id="data"  class="table table-hover table-bordered table-condensed">
                     <thead>
                         <th class="text-center">Nombre</th>
                         <th class="text-center">Descripcion</th>
                         <th class="text-center">Editar</th>
                         <th class="text-center">Eliminar</th>
                     </thead>
-                    @foreach($departamentos as $departamento)
                     <tbody>
+                        @foreach($departamentos as $departamento)
                         <tr  class="text-center">
                             <td>{{$departamento->nombre}}</td>
                             <td>{{$departamento->descripcion}}</td>
@@ -62,8 +76,8 @@
                             <td>@include('sicepla.super-admin.super-admin-deletedepartamento')
                             </td>
                         </tr>
+                        @endforeach
                     </tbody>
-                    @endforeach
                 </table>
             </div>
             {{--fin tabla--}}
@@ -76,6 +90,11 @@
   <link rel="stylesheet" href="/assets/global/plugins/bootstrap-select/css/bootstrap-select.min.css">
 @endpush
 @push('functions')
+  <!-- Datatables Scripts -->
+  <script src="{{ asset('assets/global/scripts/datatable.js') }}" type="text/javascript"></script>
+  <script src="{{ asset('assets/global/plugins/datatables/datatables.min.js') }}" type="text/javascript"></script>
+  <script src="{{ asset('assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js') }}" type="text/javascript"></script>
+  <script src="{{ asset('assets/pages/scripts/table-datatable.js') }}" type="text/javascript"></script>
   <script src="{{ asset('assets/global/plugins/jquery-validation/js/jquery.validate.min.js') }}" type="text/javascript"></script>
   <script src="{{ asset('assets/global/plugins/jquery-validation/js/additional-methods.min.js') }}" type="text/javascript"></script>
   <script src="{{ asset('assets/global/plugins/jquery-validation/js/localization/messages_es.js') }}" type="text/javascript"></script>
@@ -85,5 +104,7 @@
   <script src="/assets/global/plugins/bootstrap-select/js/bootstrap-select.min.js"></script>
   <script src="/assets/global/plugins/bootstrap-toastr/toastr.min.js"></script>
 
-
+  <script src="http://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
+          <script src="/js/DataTable.js" type="text/javascript">
+          </script>
 @endpush
